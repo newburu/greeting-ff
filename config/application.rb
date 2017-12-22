635becb363541ca5c3fd711da40d90d605cff4ee
Newburu <newburu@gmail.com>
2017-12-22 18:56:28 +0900

    configure generators

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,13 @@ Bundler.require(*Rails.groups)
 
 module GreetingFf
   class Application < Rails::Application
+        config.generators do |g|
+      g.assets false
+      g.helper false
+      g.controller_specs false
+      g.view_specs false
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
