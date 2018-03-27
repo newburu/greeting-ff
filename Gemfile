4a863b6c2d4d15ae3948a96db9332bfdd1925940
Newburu <newburu@gmail.com>
2018-03-27 19:17:56 +0900

    DBをMySQLに変更

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ end
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', groups: %w(test development), require: false
+gem 'mysql2'
 gem 'pg', groups: %w(production), require: false
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180113044432) do
 
-  create_table "before_followers", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "uid", limit: 8
+  create_table "before_followers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.bigint "user_id"
+    t.bigint "uid"
     t.string "name"
     t.string "screen_name"
     t.datetime "created_at", null: false
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 20180113044432) do
     t.index ["user_id"], name: "index_before_followers_on_user_id"
   end
 
-  create_table "followers", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "uid", limit: 8
+  create_table "followers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.bigint "user_id"
+    t.bigint "uid"
     t.string "name"
     t.string "screen_name"
     t.datetime "created_at", null: false
@@ -37,21 +37,21 @@ ActiveRecord::Schema.define(version: 20180113044432) do
     t.index ["user_id"], name: "index_followers_on_user_id"
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
+  create_table "friendly_id_slugs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "slug", null: false
     t.integer "sluggable_id", null: false
     t.string "sluggable_type", limit: 50
     t.string "scope"
     t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, length: { slug: 70, scope: 70 }
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", length: { slug: 140 }
     t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
     t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  create_table "friends", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "uid", limit: 8
+  create_table "friends", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.bigint "user_id"
+    t.bigint "uid"
     t.string "name"
     t.string "screen_name"
     t.datetime "created_at", null: false
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20180113044432) do
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
-  create_table "sessions", force: :cascade do |t|
+  create_table "sessions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "session_id", null: false
     t.text "data"
     t.datetime "created_at", null: false
@@ -68,8 +68,8 @@ ActiveRecord::Schema.define(version: 20180113044432) do
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
-  create_table "user_options", force: :cascade do |t|
-    t.integer "user_id"
+  create_table "user_options", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.bigint "user_id"
     t.boolean "dm_msg_flg"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -77,13 +77,13 @@ ActiveRecord::Schema.define(version: 20180113044432) do
     t.index ["user_id"], name: "index_user_options_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.integer "uid", limit: 8
+    t.bigint "uid"
     t.string "name"
     t.string "screen_name"
     t.string "provider"
@@ -95,4 +95,8 @@ ActiveRecord::Schema.define(version: 20180113044432) do
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
+  add_foreign_key "before_followers", "users"
+  add_foreign_key "followers", "users"
+  add_foreign_key "friends", "users"
+  add_foreign_key "user_options", "users"
 end
