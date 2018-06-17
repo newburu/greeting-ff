228e9db3098f0f2c7ef1b4d8767cb7e2ec37cfd6
Newburu <newburu@gmail.com>
2018-06-18 00:54:42 +0900

    Twitterの接続設定を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -177,8 +177,8 @@ class User < ApplicationRecord
     Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV["TWITTER_API_KEY"]
         config.consumer_secret     = ENV["TWITTER_SECRET_KEY"]
-        config.access_token        = access_token
-        config.access_token_secret = access_token_secret
+        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
     end
   end
 
