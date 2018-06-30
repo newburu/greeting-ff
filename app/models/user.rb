fd682bd9f219634126d2e0463b060aa023b96cd7
Newburu <newburu@gmail.com>
2018-06-30 09:36:29 +0900

    taskの自動更新機能時に、自分からDMにならなかったバグ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -173,12 +173,12 @@ class User < ApplicationRecord
     end
   end
 
-  def twitter_client
+  def twitter_client(user)
     Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV["TWITTER_API_KEY"]
         config.consumer_secret     = ENV["TWITTER_SECRET_KEY"]
-        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+        config.access_token        = user.try(:access_token) || ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = user.try(:access_token_secret) || ENV["TWITTER_ACCESS_TOKEN_SECRET"]
     end
   end
 
--- lib/tasks/auto_update_data.rake
+++ lib/tasks/auto_update_data.rake
@@ -4,13 +4,13 @@ namespace :auto_update_data do
   task :all_update => :environment do
     User.auto_update_users.each do |user|
       # 更新
-      user.update_followers(user.twitter_client)
-      user.update_friends(user.twitter_client)
+      user.update_followers(user.twitter_client(user))
+      user.update_friends(user.twitter_client(user))
 
       # 結果をDM連絡
-      user.send_dm(user.twitter_client)
+      user.send_dm(user.twitter_client(user))
       # 新規フォロワーさんに自動挨拶
-      user.send_new_follower_msg(user.twitter_client)
+      user.send_new_follower_msg(user.twitter_client(user))
     end
   end
 end
